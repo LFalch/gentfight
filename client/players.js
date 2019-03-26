c612a178676c586fa8c29e47c9a6ca646efdb77f
deadlap <deadlap.lucas@gmail.com>
2019-03-26 12:50:53 +0100

    characters and stuff

--- client/players.js
+++ client/players.js
@@ -1,5 +1,11 @@
 function player(side, name, character){
 
+    this.side = side;
+    this.character = character;
+    this.name = name;
+    this.lives = 10;
+    var width = 32;
+    var height = 64;
     if (side == left){
 
 
