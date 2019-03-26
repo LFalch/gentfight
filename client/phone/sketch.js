e90c38c0065540c56436fbf08b052353abc31290
LFalch <lucas@wasd.dk>
2019-03-26 18:41:55 +0100

    Add new phone functionality
    You now have to click to connect

--- client/phone/sketch.js
+++ client/phone/sketch.js
@@ -1,11 +1,15 @@
 let side;
+let socket;
 
-function setup() {
+function socketInit() {
     socket = io.connect('http://localhost:3000');
     socket.on('assign', function(data) {
         console.log('Got a side', data);
         side = data.side;
     });
+}
+
+function setup() {
     createCanvas(800, 450);
     textSize(25);
 }
@@ -13,5 +17,13 @@ function setup() {
 function draw() {
     background('white');
     fill('black');
-    text(side, 10, 30);
+    if (!socket) {
+        text('Click to join', 10, 30); 
+    } else {
+        text(side, 10, 30);
+    }
+}
+
+function mouseClicked() {
+    socketInit()
 }
--- client/sketch.js
+++ client/sketch.js
@@ -5,10 +5,15 @@ node app.js
     
 let socket;
 
-let players = [];
-
+let pLeft, pRight;
+let joined = {
+    left: false,
+    right: false,
+}
 
 function setup() {
+    pLeft = new Player('left');
+    pRight = new Player('right');
     socket = io.connect('http://localhost:3000');
     createCanvas(800, 450);
     socket.on('join', joinPlayer);
@@ -19,25 +24,24 @@ function setup() {
 function draw() {
     background('grey');
 
-    for (player of players) {
-        player.show();
+    pLeft.show();
+    pRight.show();
+
+    if (!(joined.left || joined.right)) {
+        textSize(30);
+        text("Waiting for all players", 250, 100);
     }
 }
 
 function joinPlayer(data) {
-    players.push(new Player(data.side));
+    joined[data.side] = true;
+    console.log(data.side + ' joined');
 }
 
 function actionPlayerLeft(data){
     console.log(data);
-
 }
 
 function actionPlayerRight(data){
     console.log(data);
-
 }
-
-
-
-
