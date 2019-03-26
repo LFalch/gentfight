d2858e6502df962ff5eb7dad9c40fa9ee707dd06
deadlap <deadlap.lucas@gmail.com>
2019-03-26 18:23:21 +0100

    MEH

--- client/player.js
+++ client/player.js
@@ -6,17 +6,26 @@ function imageFlip(img, x, y, w, h, sx, sy, sw, sh) {
 }
 
 function Player(side, name){
+
+    if (side == 'left'){
+        this.x = 200;
+    } else {
+        this.x = 1200;
+    }
+
     this.y = 100;
     this.sy = 0;
+    this.w = 96;
+    this.h = 192;
     this.sw = 96;
     this.sh = 192;
     if (side == 'left') {
         this.show = () => {
-            image(this.img, this.x, this.y, 96*Math.floor(this.i++ / 20), this.sy, this.sw, this.sh);
+            image(this.img, this.x, this.y, this.w, this.h, 96*Math.floor(this.i++ / 20), this.sy, this.sw, this.sh);
         };
     } else {
         this.show = () => {
-            imageFlip(this.img, this.x, this.y, 96*Math.floor(this.i++ / 20), this.sy, this.sw, this.sh);
+            imageFlip(this.img, this.x, this.y, this.w, this.h, 96*Math.floor(this.i++ / 20), this.sy, this.sw, this.sh);
         };
     }
     this.lives = 10;
