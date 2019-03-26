ac0ee2ff5243e742725d7e2576db7de0c15cd3aa
deadlap <deadlap.lucas@gmail.com>
2019-03-26 18:17:52 +0100

    idk

--- client/player.js
+++ client/player.js
@@ -7,20 +7,21 @@ function imageFlip(img, x, y, w, h, sx, sy, sw, sh) {
 
 function Player(side, name){
     this.y = 100;
-
+    this.sy = 0;
+    this.sw = 96;
+    this.sh = 192;
     if (side == 'left') {
         this.x = 200;
         this.show = () => {
-            image(this.img, this.x, this.y);
+            image(this.img, this.x, this.y, 96*Math.floor(this.i++ / 20), this.sy, this.sw, this.sh);
         };
     } else {
         this.x = 600;
         this.show = () => {
-            imageFlip(this.img, this.x, this.y);
+            imageFlip(this.img, this.x, this.y, 96*Math.floor(this.i++ / 20), this.sy, this.sw, this.sh);
         };
     }
-
     this.lives = 10;
     this.name = name;
-    this.img = loadImage('assets/character_template/idle_temp.png');
+    this.img = loadImage('assets/character_template/idle_temp_2.png');
 }
