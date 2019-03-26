ca34cba0b05e4ac50a98e5364994298911a951f8
LFalch <lucas@wasd.dk>
2019-03-26 10:56:17 +0100

    Make tabs spaces

--- client/sketch.js
+++ client/sketch.js
@@ -3,24 +3,22 @@ Skriv denne kommando i terminalen:
 node app.js
 */
 
-let socket;
+const socket;
 
 function setup() {
-	socket = io.connect('http://localhost:3000');
-	background("black");
-	createCanvas(400,400);
-	socket.on('leftPlayer', movePlayerLeft);
+    socket = io.connect('http://localhost:3000');
+    background("black");
+    createCanvas(400,400);
+    socket.on('leftPlayer', movePlayerLeft);
 }
 
 function draw() {
-	fill("red");
-	ellipse(20,20,40,40);
+    fill("red");
+    ellipse(20,20,40,40);
 }
 
-
 function movePlayerLeft(data){
-	console.log(data.x, data.y);
-
+    console.log(data.x, data.y);
 }
 
 
